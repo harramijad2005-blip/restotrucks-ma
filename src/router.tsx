ad96ca6f8c959da373844edc0a346a31d043a222
harramijad2005-blip <harramijad2005@gmail.com>
2026-07-15 09:53:16 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,7 @@ import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
-
+{ rel: "icon", type: "image/png", href: "/favicon.png?v=2" },
   const router = createRouter({
     routeTree,
     context: { queryClient },
